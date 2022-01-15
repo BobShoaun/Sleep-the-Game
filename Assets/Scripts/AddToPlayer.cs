3e3f92f796a504c565af2a65c1973220748a5986
Allenzxz <35384995+Allenzxz@users.noreply.github.com>
2022-01-15 14:58:56 -0500

    sleep meter v2

--- Assets/Scripts/AddToPlayer.cs
+++ Assets/Scripts/AddToPlayer.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class AddToPlayer : MonoBehaviour
 {
-    private bool _spacePressed;
+    private bool _jPressed;
     private Rigidbody _rigidbody;
     private Transform _transform;
     public SleepMeter SleepMeter;
+
+    private bool sleeping = false;
+
+    private bool interrupted;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,21 +23,68 @@ public class AddToPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _spacePressed = Input.GetKeyDown("J");
+        _jPressed = Input.GetKeyDown("J");
     }
-
     private void FixedUpdate()
     {
-        if (_spacePressed)
+        if (_jPressed)
         {
+            interrupted = false;
+            sleeping = true;
             float timeLeft = 3.0f;
             transform.RotateAround(_transform.position, Vector3.forward, 180);
             timeLeft -= Time.deltaTime;
+
+            Collider[] hitColliders = Physics.OverlapSphere(_transform.position, 3);
+            // for (int i = 0; i < sizeof(hitColliders); i++) {
+            //     if (hitColliders[i].tag == "partypeople"){
+            //         timeLeft = -1;
+            //         sleeping = false;
+            //         break;
+            //     }
+            // }
+            if (hitColliders) 
+            {
+                timeLeft = -1;
+                sleeping = false;
+            }
+
             if (timeLeft < 0) 
             {
-                SleepMeter.UpdateSleepMeter();
+                if (sleeping)
+                {
+                    SleepMeter.UpdateSleepMeter(); 
+                    sleeping = false;
+                }
                 transform.RotateAround(_transform.position, Vector3.back, 180);
             }
         }
     }
 }
+
+
+
+// public class Example : MonoBehaviour
+// {
+//     //Assign a GameObject in the Inspector to rotate around
+//     public GameObject target;
+
+//     void Update()
+//     {
+//         // Spin the object around the target at 20 degrees/second.
+//         transform.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+//     }
+// }
+
+// public class TimerTest : MonoBehaviour {
+//     [SerializeField] private float _duration = 1f;
+//     private float _timer = 0f;
+ 
+//     private void Update() {
+//         _timer += Time.deltaTime;
+//         if (_timer >= _duration) {
+//             _timer = 0f;
+//             // Do Stuff here
+//         }
+//     }
+// }
